03b26d48e11eeb4307eef6270d519f3194f06604
CyanFlxy <xia_yuqiang@163.com>
2015-09-06 20:38:19 +0800

    工程框架

---
+++ MagicTower/src/main/java/com/github/cyanflxy/alien/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.cyanflxy.alien;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
